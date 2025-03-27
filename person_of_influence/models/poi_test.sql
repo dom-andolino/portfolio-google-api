8ba32673c1050a67c8459292fd01f507366f126a
dom-andolino <42012173+dom-andolino@users.noreply.github.com>
2025-03-26 21:21:19 -0400

    define source/raw tables in dbt schema and reference in model SQL so shows in docs/DAG/leneage

--- person_of_influence/models/poi_test.sql
+++ person_of_influence/models/poi_test.sql
@@ -1,6 +1,6 @@
 select 
     dt,
     count(*) as num_rows
-from poi_stage
+from {{ source('person_of_influence', 'poi_stage') }} 
 group by dt
 order by dt
--- person_of_influence/models/poi_top_curr.sql
+++ person_of_influence/models/poi_top_curr.sql
@@ -3,11 +3,11 @@ SELECT
     name,
     num_cats,
     best_cat_rnk
-FROM poi_hist
+FROM {{ source('person_of_influence', 'poi_hist') }} 
 WHERE   
     dt = (select max(dt) from poi_hist)
     and 
     (
         num_cats >= 2
-        or best_cat_rnk <50
+        or best_cat_rnk <=50
     )
